191821a64fca5a84bffe8d3f45c48088c1c3563a
Joe Carpinelli <joseph.d.carpinelli@loopy.software>
2023-04-30 22:31:35 -0500

    Support for discrete time points

--- src/HorizonsEphemeris.jl
+++ src/HorizonsEphemeris.jl
@@ -80,10 +80,10 @@ function ephemeris(
     body, start, stop, intervol;
     site="",
     wrt="ssb",
-    epoch="J2000",
+    timeformat="MJD",
     file=nothing,
-    units="KM-S",
-    headers=[:MJD, :Calendar, :X, :Y, :Z, :ΔX, :ΔY, :ΔZ]
+    units="AU-D",
+    header=[:MJD, :Calendar, :X, :Y, :Z, :ΔX, :ΔY, :ΔZ]
 )
     code = body isa Integer ? body : NAIF(body)
 
@@ -97,7 +97,6 @@ function ephemeris(
         START_TIME=Dates.format(DateTime(start), "yyyy-mm-dd HH:MM:SS.sss"),
         STOP_TIME=Dates.format(DateTime(stop), "yyyy-mm-dd HH:MM:SS.sss"),
         STEP_SIZE=string(intervol),
-        REF_SYSTEM=epoch,
         REF_PLANE="FRAME",
         CSV_FORMAT=true,
         VEC_TABLE=2,
@@ -106,7 +105,7 @@ function ephemeris(
         VEC_LABELS=false,
         VEC_DELTA_T=false,
         TIME_DIGITS="FRACSEC",
-        TLIST_TYPE="MJD"
+        TLIST_TYPE=timeformat
     )
 
     content = String(response.body)
@@ -118,10 +117,60 @@ function ephemeris(
     notes = strip(notes)
 
     csv = CSV.File(IOBuffer(ephemeris); header=false, drop=[9])
-    output = NamedTuple(label => csv[column] for (label, column) in zip(headers, csv.names))
+    output = NamedTuple(label => csv[column] for (label, column) in zip(header, csv.names))
 
     if !isnothing(file)
-        CSV.write(file, csv; headers=headers)
+        CSV.write(file, csv; header=header)
+        @info "Ephemeris data for object with NAIF ID $code has been written to $file."
+    end
+
+    return output
+end
+
+
+function ephemeris(
+    body, times;
+    site="",
+    wrt="ssb",
+    timeformat="MJD",
+    file=nothing,
+    units="AU-D",
+    header=[:MJD, :Calendar, :X, :Y, :Z, :ΔX, :ΔY, :ΔZ]
+)
+    code = body isa Integer ? body : NAIF(body)
+
+    if !(uppercase(strip(units)) in ("AU-D", "KM-D", "KM-S"))
+        throw(ErrorException("The only acceptable inputs for the units keyword argument are: KM-S, KM-D, AU-D."))
+    end
+    response = fetch_vectors(
+        code;
+        format="text",
+        CENTER="$(site)@$(NAIF(wrt))",
+        TLIST=times,
+        TLIST_TYPE=timeformat,
+        REF_PLANE="FRAME",
+        CSV_FORMAT=true,
+        VEC_TABLE="2",
+        VEC_CORR="NONE",
+        OUT_UNITS=units,
+        VEC_LABELS=false,
+        VEC_DELTA_T=false,
+        TIME_DIGITS="FRACSEC"
+    )
+
+    content = String(response.body)
+    _, ephemeris = split(content, "\$\$SOE")
+    ephemeris, notes = split(ephemeris, "\$\$EOE")
+
+    ephemeris = strip(ephemeris)
+
+    notes = strip(notes)
+
+    csv = CSV.File(IOBuffer(ephemeris); header=false, drop=[9])
+    output = NamedTuple(label => csv[column] for (label, column) in zip(header, csv.names))
+
+    if !isnothing(file)
+        CSV.write(file, csv; header=header)
         @info "Ephemeris data for object with NAIF ID $code has been written to $file."
     end
 
