2312a6bf6f898c836482d2e65ed1fc10e753b138
Joey Carpinelli <joey@loopy.codes>
2024-03-23 22:30:03 -0400

    Switching to non-multidoc format

--- docs/make.jl
+++ docs/make.jl
@@ -1,51 +1,25 @@
+using Latexify
+using Documenter
+using ModelingToolkit
+using AstrodynamicalModels
 
-using MultiDocumenter:
-    DropdownNav,
-    MultiDocRef,
-    MegaDropdownNav,
-    Column,
-    make,
-    SearchConfig,
-    FlexSearch
-
-clonedir = mktempdir()
-
-ref(name) = MultiDocRef(
-    upstream=joinpath(clonedir, name),
-    path=name,
-    name=name,
-    giturl="https://github.com/cadojo/$name.jl.git",
+makedocs(
+    sitename="`ephemeris.loopy.codes`",
+    format=Documenter.HTML(),
+    modules=[],
+    pages=[
+        "Home" => "index.md",
+        "Overview" => [
+            "Getting Started" => "getting-started/index.md",
+            "Examples" => "examples/index.md",
+        ],
+    ]
 )
 
-horizons = [
-    ref("HorizonsAPI")
-]
-spice = [
-    ref("SPICEKernels")
-]
-
-docs = [
-    # MultiDocRef(
-    #     upstream=joinpath(clonedir, "Home"),
-    #     path="Overview",
-    #     name="Home",
-    #     giturl="https://github.com/cadojo/ephemeris.loopy.codes.git"
-    # ),
-    MegaDropdownNav(
-        "Ephemeris Sources", [
-            Column("JPL HORIZONS", horizons),
-            Column("JPL SPICE Kernels", spice),
-        ]
-    )
-]
-
-outpath = joinpath(@__DIR__, "build")
-
-make(
-    outpath,
-    docs;
-    search_engine=SearchConfig(
-        index_versions=["stable", "dev"],
-        engine=FlexSearch
-    )
+deploydocs(
+    target="build",
+    repo="github.com/cadojo/ephemeris.loopy.codes.git",
+    branch="gh-pages",
+    devbranch="main",
+    versions=["stable" => "v^", "manual", "v#.#", "v#.#.#"],
 )
