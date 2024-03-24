30fc6f3c9955cf864ab84847071f8ba93e35dda3
Documenter.jl <documenter@juliadocs.github.io>
2024-03-24 03:04:09 +0000

    build based on 4d027db

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"getting-started/#Getting-Started","page":"Getting Started","title":"Getting Started","text":"","category":"section"},{"location":"examples/#Examples","page":"Examples","title":"Examples","text":"","category":"section"}]
+}
