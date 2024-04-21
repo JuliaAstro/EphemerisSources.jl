5e82fad5877301a79a6455e5de81c0f8eb261d72
Joey Carpinelli <joey@loopy.codes>
2024-04-21 13:06:11 -0400

    Updating documenterion; version bump

--- docs/make.jl
+++ docs/make.jl
@@ -2,15 +2,19 @@ using Documenter
 using SPICEBodies
 
 makedocs(
-    sitename="SPICEBodies",
-    format=Documenter.HTML(),
-    modules=[SPICEBodies]
+    sitename = "SPICEBodies",
+    format = Documenter.HTML(),
+    modules = [SPICEBodies],
+    pages = [
+        "Overview" => "index.md",
+        "Reference" => "reference/index.md"
+    ]
 )
 
 deploydocs(
-    target="build",
-    repo="github.com/cadojo/SPICEBodies.jl.git",
-    branch="gh-pages",
-    devbranch="main",
-    versions=["stable" => "v^", "manual", "v#.#", "v#.#.#"],
+    target = "build",
+    repo = "github.com/cadojo/SPICEBodies.jl.git",
+    branch = "gh-pages",
+    devbranch = "main",
+    versions = ["stable" => "v^", "manual", "v#.#", "v#.#.#"]
 )
