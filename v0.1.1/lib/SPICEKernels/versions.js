f94380bcf2b09723c61155de7795acff12e66996
Joey Carpinelli <joseph.d.carpinelli@loopy.codes>
2024-08-13 20:22:32 -0400

    Removes all docs

--- v0.1.1/lib/SPICEKernels/versions.js
+++
@@ -1,7 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v1.4",
-  "dev",
-];
-var DOCUMENTER_NEWEST = "v1.4.0";
-var DOCUMENTER_STABLE = "stable";
--- v0.2.0/lib/SPICEBodies/versions.js
+++
@@ -1,7 +0,0 @@
-var DOC_VERSIONS = [
-  "stable",
-  "v1.0",
-  "dev",
-];
-var DOCUMENTER_NEWEST = "v1.0.0";
-var DOCUMENTER_STABLE = "stable";
