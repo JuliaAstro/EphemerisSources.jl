730747c4533c17bb26b0ac03758a1d83b2170779
Joey Carpinelli <joseph.d.carpinelli@loopy.codes>
2024-09-12 21:08:01 -0400

    Adds test for AstroTime integration

--- lib/HorizonsEphemeris/test/runtests.jl
+++ lib/HorizonsEphemeris/test/runtests.jl
@@ -1,4 +1,4 @@
-using Test, Dates, HorizonsEphemeris
+using Test, Dates, HorizonsEphemeris, AstroTime, DataFrames
 
 @testset "Simple Request" begin
     @test ephemeris("moon", "2022-01-01", "2023-01-01", "1 month") isa NamedTuple
@@ -14,8 +14,23 @@ end
     @test ephemeris("earth", mars.t) isa NamedTuple
 end
 
+@testset "External Package Integration" begin
+    # AstroTime (dependency)
+    ep = TTEpoch(59235.0days, origin = :modified_julian)
+
+    earth = let start = ep - 50years, stop = ep + 50years, step = 1days
+        @time ephemeris("earth", start, stop, step; wrt = "jupiter", units = "AU-D")
+        # what are my units choices, what are my named bodies options?
+    end
+
+    @test earth isa NamedTuple
+
+    # DataFrames (not a dependency)
+    @test DataFrame(earth) isa DataFrame
+end
+
 @testset "Write to File" begin
     file = tempname()
-    ephemeris("jupiter", now(); file=file)
+    ephemeris("jupiter", now(); file = file)
     @test isfile(file)
 end
